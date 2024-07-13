2e4b5c58f31d55e181757f3997208585d435ad98
Ananthasetty Himakar <himakarananthasetty@gmail.com>
2024-07-13 01:28:03 -0500

    Added cloud front url to allowed cors origin.

--- backend/crud/settings.py
+++ backend/crud/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS =[
     'http://localhost:3000',
     'http://recordmanagementsystem-react.s3-website.us-east-2.amazonaws.com',
+     'https://d38gztwwel0vi9.cloudfront.net/',
+
 ]
 
 ROOT_URLCONF = "crud.urls"
